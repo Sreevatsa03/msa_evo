4f0eeab2d7ac58665f3bf7387d6304312d49749d
rootma21 <utamrach@gmail.com>
2022-04-25 22:07:52 -0400

    evo app

--- evo_app.py
+++ evo_app.py
@@ -1,6 +1,5 @@
 from EvoAlign import EvoAlign, Evo
 
-
 dash = 'data/dash.fasta'
 dog = 'data/canis_lupus_familiaris.fasta'
 human = 'data/homo_sapiens.fasta'
@@ -8,7 +7,14 @@ mouse = 'data/mus_musculus.fasta'
 all = 'data/all.fasta'
 fastas = [dog, human, mouse]
  
-
+# create alignment environment
 evo_a = EvoAlign()
+
+# load the fasta file
 evo_a.read_fasta(all)
-evo_a.align(gens=2000, dom=100, status=100)
+
+# run alignment and get visualizations
+evo_a.align(gens=2000, dom=100, status=100, show=True)
+
+# saves alignment to fasta
+evo_a.save_alignment()
